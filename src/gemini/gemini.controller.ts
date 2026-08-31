684a386f113b255376662d3de86875b12826a164
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2026-08-30 19:03:53 -0600

    Estructura básica

---
+++ src/gemini/gemini.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { GeminiService } from './gemini.service';
+
+@Controller('gemini')
+export class GeminiController {
+  constructor(private readonly geminiService: GeminiService) {}
+
+  @Get()
+  getHelloWorld() {
+    return 'Hola mundo';
+  }
+}
