51b1e48ac3d6e8a022fb0633276b6cb4a696daa1
Marcelo Fuentes <mfuentes@fedecovera.com>
2026-08-31 15:30:52 -0600

    Preparación de backend

--- src/gemini/gemini.service.ts
+++ src/gemini/gemini.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class GeminiService {}
+export class GeminiService {
+  basicPrompt() {
+    return { hola: 'saludos' };
+  }
+}
