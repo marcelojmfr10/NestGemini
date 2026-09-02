fa6f51d4cd1f8a97cb9699aa9bf4dae1e6c9ad46
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2026-09-01 19:27:31 -0600

    Subir, leer y enviar a gemini imágenes

--- src/gemini/use-cases/basic-prompt-stream.use-case.ts
+++ src/gemini/use-cases/basic-prompt-stream.use-case.ts
@@ -1,4 +1,4 @@
-import { GoogleGenAI } from '@google/genai';
+import { createUserContent, GoogleGenAI } from '@google/genai';
 import { BasicPromptDto } from '../dtos/basic-prompt.dto';
 
 interface Options {
@@ -11,13 +11,36 @@ export const basicPromptStreamUseCase = async (
   basicPromptDto: BasicPromptDto,
   options?: Options,
 ) => {
+  const files = basicPromptDto.files;
+
+  const images = await Promise.all(
+    files.map(async (file) => {
+      return await ai.files.upload({
+        file: new Blob([file.buffer], {
+          type: file.mimetype.includes('image') ? file.mimetype : 'image/jpg',
+        }),
+      });
+    }),
+  );
+
   const {
     model = 'gemini-3.6-flash',
     systemInstruction = `Responde únicamente en español, en formato markdown, usa negritas de esta forma __, usa el sistema métrico decimal`,
   } = options ?? {};
   const stream = await ai.interactions.create({
     model: model,
-    input: basicPromptDto.prompt,
+    // input: basicPromptDto.prompt,
+    input: [
+      {
+        type: 'text',
+        text: basicPromptDto.prompt,
+      },
+      ...images.map((image) => ({
+        type: 'image' as const,
+        uri: image.uri,
+        mime_type: image.mimeType,
+      })),
+    ],
     system_instruction: systemInstruction,
     stream: true,
   });
